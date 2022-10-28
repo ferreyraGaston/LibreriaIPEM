6235f7c671fd2d22f7fcd4828358840f0ffe5336
gastonloco <ferreyragaston351@gmail.com>
2022-10-28 00:48:57 -0300

    color white dialog

--- Solucion/Video Club/Dialogo.Designer.cs
+++ Solucion/Video Club/Dialogo.Designer.cs
@@ -57,10 +57,10 @@
             this.icon.BackColor = System.Drawing.SystemColors.Control;
             this.icon.Enabled = false;
             this.icon.Image = ((System.Drawing.Image)(resources.GetObject("icon.Image")));
-            this.icon.Location = new System.Drawing.Point(130, 2);
+            this.icon.Location = new System.Drawing.Point(108, 2);
             this.icon.Name = "icon";
-            this.icon.Size = new System.Drawing.Size(163, 114);
-            this.icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.icon.Size = new System.Drawing.Size(211, 170);
+            this.icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.icon.TabIndex = 0;
             this.icon.TabStop = false;
             // 
@@ -77,7 +77,7 @@
             this.btnOk.ActiveFillColor = System.Drawing.Color.SeaGreen;
             this.btnOk.ActiveForecolor = System.Drawing.Color.White;
             this.btnOk.ActiveLineColor = System.Drawing.Color.SeaGreen;
-            this.btnOk.BackColor = System.Drawing.SystemColors.Control;
+            this.btnOk.BackColor = System.Drawing.Color.White;
             this.btnOk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOk.BackgroundImage")));
             this.btnOk.ButtonText = "OK";
             this.btnOk.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -88,7 +88,7 @@
             this.btnOk.IdleFillColor = System.Drawing.Color.White;
             this.btnOk.IdleForecolor = System.Drawing.Color.SeaGreen;
             this.btnOk.IdleLineColor = System.Drawing.Color.SeaGreen;
-            this.btnOk.Location = new System.Drawing.Point(140, 180);
+            this.btnOk.Location = new System.Drawing.Point(143, 215);
             this.btnOk.Margin = new System.Windows.Forms.Padding(5);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(140, 54);
@@ -102,7 +102,7 @@
             this.lbDialog.AutoSize = true;
             this.lbDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbDialog.ForeColor = System.Drawing.Color.Green;
-            this.lbDialog.Location = new System.Drawing.Point(62, 140);
+            this.lbDialog.Location = new System.Drawing.Point(61, 185);
             this.lbDialog.Name = "lbDialog";
             this.lbDialog.Size = new System.Drawing.Size(301, 25);
             this.lbDialog.TabIndex = 2;
@@ -119,8 +119,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(420, 248);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(425, 283);
             this.Controls.Add(this.lbDialog);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.icon);
--- Solucion/Video Club/Reservas.Designer.cs
+++ Solucion/Video Club/Reservas.Designer.cs
@@ -35,7 +35,7 @@ namespace Video_Club
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(77, 70);
+            this.button1.Location = new System.Drawing.Point(80, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 98);
             this.button1.TabIndex = 0;
